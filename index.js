1853d89737bb137d22f194d50550160d5e0fb771
Sivan Mehta <smehta@godaddy.com>
2019-04-23 10:33:41 -0700

    it works

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function parallel(tasks, callback) {
-  
+async function parallel(fns, callback = () => {}) {
+  const tasks = fns.map(f => {
+    return new Promise((resolve, reject) => {
+      f((err, result) => {
+        if(err) reject(err);
+        resolve(result);
+      });
+    });
+  });
+
+  let results = [];
+  try {
+    results = await Promise.all(tasks);
+  } catch (err) {
+    return callback(err);
+  }
+
+  callback(null, results);
 }
 
 module.exports = parallel;
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,25 @@
 const assume = require('assume');
 const parallel = require('../');
 
-describe('signature', function () {
+describe('parallel', function () {
   it('is a function', function () {
-    assume(parallel).is.a('function');
-    assume(parallel).has.length(2);
+    assume(parallel).is.a('asyncfunction');
+    assume(parallel).has.length(1);
+  });
+
+  it('takes less than 4 seconds to run lots of 2 second tasks', function (done) {
+    this.timeout(4000);
+    const start = Date.now();
+    parallel([
+      callback => setTimeout(() => callback(null, 'one'), 2000),
+      callback => setTimeout(() => callback(null, 'two'), 2000),
+      callback => setTimeout(() => callback(null, 'three'), 2000),
+      callback => setTimeout(() => callback(null, 'four'), 2000),
+      callback => setTimeout(() => callback(null, 'five'), 2000),
+      callback => setTimeout(() => callback(null, 'six'), 2000)
+    ], (err, results) => {
+      assume(results).deep.equals([ 'one', 'two', 'three', 'four', 'five', 'six' ]);
+      done();
+    });
   });
 });
